c9654fa56c24118c3bce9f72072ef1a360adf2bd
Jeremy Reed <reeje76@gmail.com>
2024-07-22 15:13:16 -0400

    Naive wallpaper loading.
    Loads all wallpapers in connection into memory.
    This eats up a huge amount of memory.
    We will need a thumbnailing service.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
+import 'package:walls/wallpaper_collection_loader.dart';
 
 void main() {
   runApp(const MyApp());
@@ -55,17 +58,29 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _numWallpapers = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  Future<void> _loadWallpapers() async {
+    debugPrint('Loading wallpapers...');
+
+    List<Uint8List> wallpapers = [];
+    Directory collectionRoot = Directory('/home/jeremyr/Pictures/Wallpapers/');
+
+    await for (final FileSystemEntity entity in collectionRoot.list()) {
+      if (entity is File) {
+        final Uint8List data = await entity.readAsBytes();
+
+        if (await WallpaperCollectionLoader.isSupportedImage(data)) {
+          wallpapers.add(data);
+
+          setState(() {
+            _numWallpapers = wallpapers.length;
+          });
+        }
+      }
+    }
+
+    debugPrint('Loaded all ${wallpapers.length} wallpapers.');
   }
 
   @override
@@ -87,37 +102,14 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+        child: Text('Wallpapers: $_numWallpapers'),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {
+          debugPrint('You pressed the button!');
+          _loadWallpapers();
+        },
+        tooltip: 'You know you want to press this button...',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
