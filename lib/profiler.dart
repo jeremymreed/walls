0e4ef8a8210bb4cd4f8163ff5dfc032da8200856
Jeremy Reed <reeje76@gmail.com>
2024-10-12 16:21:08 -0400

    Add profiler.

---
+++ lib/profiler.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/foundation.dart';
+
+class Profiler {
+  DateTime? start;
+
+  Profiler() {
+    debugPrint('Starting Profiler');
+    start = DateTime.now();
+  }
+
+  void startProfiling(String message) {
+    debugPrint('Started Profiler: $message');
+    start = DateTime.now();
+  }
+
+  Duration getDifferenceStartNow(String message) {
+    if (start != null) {
+      final end = DateTime.now();
+      final duration = end.difference(start!);
+      debugPrint('$message: Duration: $duration');
+      return duration;
+    } else {
+      throw Exception('Profiler not started.');
+    }
+  }
+}
