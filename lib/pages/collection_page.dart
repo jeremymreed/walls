66274f3e34973571a2636ed4445f2cfe61e7240e
Jeremy Reed <reeje76@gmail.com>
2024-10-12 16:23:07 -0400

    Show CollectionImporter.
    When the database is empty, show the CollectionImporter widget.

--- lib/pages/collection_page.dart
+++ lib/pages/collection_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:walls/main.dart';
+import 'package:walls/pages/importer_page.dart';
 
 class CollectionPage extends StatelessWidget {
   const CollectionPage({super.key});
@@ -15,7 +16,7 @@ class CollectionPage extends StatelessWidget {
             if (wallpaperEntries.isNotEmpty) {
               return const Text('Gallery');
             } else {
-              return const Text('Importer');
+              return const CollectionImporter();
             }
           } else if (snapshot.hasError) {
             return Text('Error: ${snapshot.error}');
