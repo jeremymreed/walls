77a988dc4b53a808eb635aa153a5a64c3028b111
Jeremy Reed <reeje76@gmail.com>
2024-10-12 13:33:17 -0400

    Add database initialization code.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,45 @@
+import 'dart:io';
+import 'package:args/args.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:flutter/material.dart';
+import 'package:xdg_directories/xdg_directories.dart' as xdg;
+import 'package:walls/config.dart';
+import 'package:walls/local_database.dart';
 import 'package:walls/pages/collection_page.dart';
 
-void main() {
+// We're supporting only Linux, so this hardcoded path is ok.
+final String dbPathBase = '${xdg.dataHome.path}/walls';
+late final LocalDatabase db;
+
+void main(List<String> arguments) async {
+  if (Platform.isLinux) {
+    sqfliteFfiInit();
+  } else {
+    debugPrint('Platform is not Linux');
+    exit(1);
+  }
+  databaseFactory = databaseFactoryFfi;
+
+  ArgParser parser = ArgParser();
+  parser.addOption('dbFilename',
+      defaultsTo: 'database.sqlite', help: 'Filename of the SQLite database');
+  parser.addFlag('help',
+      abbr: 'h', negatable: false, help: 'Print this help message');
+
+  var results = parser.parse(arguments);
+
+  if (results['help']) {
+    debugPrint(parser.usage);
+    exit(0);
+  }
+
+  Config config = Config(
+      dbPath: '$dbPathBase/${results.option('dbFilename')!}',
+      collectionPath: '');
+
+  db = LocalDatabase(dbPath: config.dbPath);
+
+  await db.openLocalDatabase();
   runApp(const MyApp());
 }
 
