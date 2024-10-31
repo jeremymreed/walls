3839e3bf495666f570b88b297ff88dba41b804cb
Jeremy Reed <reeje76@gmail.com>
2024-10-31 15:23:50 -0400

    Add output selection dropdown.

--- lib/pages/output_status/output_status_page.dart
+++ lib/pages/output_status/output_status_page.dart
@@ -12,9 +12,23 @@ class OutputStatusPage extends StatefulWidget {
 class _OutputStatusPageState extends State<OutputStatusPage> {
   late Future<GetOutputsSettingsResponse> _outputsSettingsFuture;
 
+  late List<DropdownMenuEntry<String>> _outputItems;
+
+  List<DropdownMenuEntry<String>> _buildDropdownMenuItems(
+      List<OutputSetting> settings) {
+    return settings.map((setting) {
+      return DropdownMenuEntry<String>(
+        label: setting.name,
+        value: setting.name,
+      );
+    }).toList();
+  }
+
   @override
   void initState() {
+    super.initState();
     _outputsSettingsFuture = sendGetOutputsSettings();
+    _outputItems = <DropdownMenuEntry<String>>[];
     super.initState();
   }
 
@@ -23,7 +37,31 @@ class _OutputStatusPageState extends State<OutputStatusPage> {
     return FutureBuilder(
       future: _outputsSettingsFuture,
       builder: (context, snapshot) {
-        return const Text('Testing!');
+        if (snapshot.hasData && snapshot.data != null) {
+          debugPrint('snapshot.data: ${snapshot.data}');
+          _outputItems = _buildDropdownMenuItems(snapshot.data!.settings);
+
+          if (_outputItems.isEmpty) {
+            return const Text('No data');
+          }
+
+          return Center(
+            child: DropdownMenu<String>(
+              initialSelection: _outputItems[0].value,
+              dropdownMenuEntries: _outputItems,
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        } else if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: SizedBox(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        } else {
+          return const Text('No data!');
+        }
       },
     );
   }
