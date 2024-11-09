5f6312d90f6d2fd31830423dbde947d658b89ba0
Jeremy Reed <reeje76@gmail.com>
2024-11-09 14:16:27 -0500

    Convert OutputStatus to a stateless widget.

--- lib/widgets/output_status.dart
+++ lib/widgets/output_status.dart
@@ -3,21 +3,11 @@ import 'package:walls/services/wallsd/get_outputs_settings_response_mapper.dart'
 import 'package:walls/enums/thumbnail_flavor_enum.dart';
 import 'package:walls/widgets/thumbnail.dart';
 
-class OutputStatus extends StatefulWidget {
+class OutputStatus extends StatelessWidget {
   final OutputSetting _settings;
 
   const OutputStatus({super.key, required OutputSetting settings})
       : _settings = settings;
-
-  @override
-  State<OutputStatus> createState() => _OutputStatusState();
-}
-
-class _OutputStatusState extends State<OutputStatus> {
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,8 +16,7 @@ class _OutputStatusState extends State<OutputStatus> {
         children: [
           const SizedBox(height: 10),
           Thumbnail(
-              imagePath: widget._settings.wallpaper,
-              flavor: ThumbnailFlavor.x_large),
+              imagePath: _settings.wallpaper, flavor: ThumbnailFlavor.x_large),
         ],
       ),
     );
